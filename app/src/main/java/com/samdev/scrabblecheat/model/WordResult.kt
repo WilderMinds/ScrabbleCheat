413627adb6938e295d836b09843444987b430ae2
elsammyk@gmail.com <elsammyk@gmail.com>
2023-10-02 00:10:06 +0000

    spck text

--- app/src/main/java/com/samdev/scrabblecheat/model/WordResult.kt
+++ app/src/main/java/com/samdev/scrabblecheat/model/WordResult.kt
@@ -1,6 +1,6 @@
 package com.samdev.scrabblecheat.model
 
-data class WordResult(
+internal data class WordResult(
     val word: String,
     val score: Int,
     val readableScore: String = "$score pts"
